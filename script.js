8df154249e98793f0212fdcfc09fc81136c6b454
Lukas Andersen <luke@slate71.com>
2016-12-10 02:27:26 -0800

    Show solved puzzle onload and scramble pieces onclick

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id);
+// function allowDrop(event) {
+//     event.preventDefault();
+// }
+
+// function drop(event) {
+//     event.preventDefault();
+//     var data = event.dataTransfer.getData("Text");
+//     event.target.appendChild(document.getElementById(data));
+// }
+
+function dragstart_handler(event) {
+  console.log(event);
+  event.dataTransfer.setData("text/plain", event.target.id);
 }
 
-
-function allowDrop(event) {
-    event.preventDefault();
+function drop_handler(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text/plain");
+  event.target.appendChild(document.getElementById(data));
 }
 
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-    
-       
+function dragover_handler(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "move";
+}
+
+function click_handler(event) {
+  var bg = document.querySelector('#droppableZone');
+  var pieces = [].slice.call(document.querySelectorAll('.piece'));
+
+  bg.style.backgroundImage = "url('images/_Puzzle_bg_unsolved.png')";
+
+  pieces.forEach(piece => {
+    piece.style.display = 'block'
+    piece.style.position = 'absolute';
+    piece.style.top = Math.floor(Math.random() * (48+1)) + '%';
+    piece.style.left = Math.floor(Math.random() * (87+1)) + '%';
+    document.querySelector('#pieces-tray').appendChild(piece);
+  });
 }
