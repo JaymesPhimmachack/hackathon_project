1bf6fd2b0b45135b86a4c20b5c2d0c069a31de87
Joseph Michael Matembu <jmatembu@gmail.com>
2016-12-11 20:49:40 +0300

    Add ability to choose difficulty

--- script.js
+++ script.js
@@ -1,3 +1,91 @@
+/* *******************************************************
+ *
+ * JavaScript implementation for the Hexagon Jigsaw Puzzle,
+ * a hackathon project at Learn.ModernDeveloper
+ * - https://learn.moderndeveloper.com
+ *
+ * Authors:
+ * 1. Denine Guy
+ * 2. Jaymes Phimmachack
+ * 3. Joseph M. Matembu
+ * 4. Lukas Andersen
+ *
+ * *******************************************************
+ */
+
+
+
+/***** Objects *****/
+
+var settings = {};
+
+/***** Selecting DOM Elements *****/
+
+var easyButton = document.getElementById('easy');
+var mediumButton = document.getElementById('medium');
+var hardButton = document.getElementById('hard');
+var scramble = document.getElementById('scramble');
+var elementsToShake = document.querySelectorAll('.piece');
+var buttonsToHide = [easyButton, mediumButton, hardButton, scramble];
+/***** Functions *****/
+
+var hideStartButtons = function(buttons) {
+
+  return buttons.forEach(function(element, index) {
+    return element.setAttribute("class", "hide-element");
+  });
+
+};
+
+/*
+ * Shake the elements provided.
+ * Parameter 1: Array - An array of elements to apply the animations to
+ */
+var shakeElements = function(elements) {
+  /* Will store a randomly generated integer value,
+   * needed in picking a random animation.
+   */
+  var randomIndex = 0;
+  var animations = ["animate", "animate_rotate", "animate_horizontal", "animate_vertical", "animate_crazy"];
+
+  function shake(elementToShake, index) {
+    // Generate random integer between 0 and the length of the array of animations.
+    randomIndex = Math.floor(Math.random() * (animations.length));
+    // Add a shake animation class to element.
+    elementToShake.classList.add(animations[randomIndex]);
+  }
+  // Call the shake function on each element.
+  elements.forEach(shake);
+};
+// Puzzle has been solved by gamer.
+var puzzleSolved = function() {
+  setTimeout(shakeElements, settings.durationBeforeCongs, elementsToShake);
+}
+
+
+
+// Some settings to get us started.
+settings.durationBeforeCongs = 3000; // In milliseconds
+settings.difficulty = "easy"; // To be changed by user if they click the difficulty buttons
+settings.durations = {"easy": 300, "medium": 150, "hard": 60}; // In seconds
+settings.getDuration = function() {
+  return this.durations[this.difficulty];
+};
+// Change the difficulty level when user clicks any of the difficulty buttons
+settings.setDifficulty = function(value) {
+  return this.difficulty = value;
+};
+
+// Get the difficulty selected by the user
+easyButton.addEventListener('click', function() {
+  settings.setDifficulty("easy");
+}, false);
+mediumButton.addEventListener('click', function() {
+  settings.setDifficulty("medium");
+}, false);
+hardButton.addEventListener('click', function() {
+  settings.setDifficulty("hard");
+}, false);
 
 
 
@@ -37,8 +125,8 @@ function clickHandler(event) {
   });
 
   showControlsAndTimer();
-  hideScramble();
-  startClock(5);
+  hideStartButtons(buttonsToHide);
+  startClock(settings.getDuration());
 }
 
 // Show and Hide Buttons
@@ -53,10 +141,10 @@ function showControlsAndTimer() {
   clock.setAttribute("class", "timer");
 }
 
-function hideScramble() {
-  var scramble = document.getElementById('scramble');
-  scramble.setAttribute("class", "hide-element");
-}
+// function hideScramble() {
+//   var scramble = document.getElementById('scramble');
+//   scramble.setAttribute("class", "hide-element");
+// }
 
 // Show and Hide Hint
 function showHint(event) {
@@ -110,42 +198,9 @@ function playSound() {
   document.getElementById('audiotag1').play();
 }
 
-// Congradulate User
-
-(function() {
-  
-  
-  var shakeElements = function(elements) {
-    /* Will store a randomly generated integer value,
-     * needed in picking a random animation.
-     */
-    var randomIndex = 0;
-    var elementsToShake = document.querySelectorAll('.piece');
-    var animations = ["animate", "animate_rotate", "animate_horizontal", "animate_vertical", "animate_crazy"];
-    
-    function shake(elementToShake, index) {
-      // Generate random integer between 0 and the length of the array of animations.
-      randomIndex = Math.floor(Math.random() * (animations.length));
-      // Add a shake animation class to element.
-      elementToShake.classList.add(animations[randomIndex]);
-    }
-    // Call the shake function on each element.
-    elements.forEach(shake);
-  };
-  // Puzzle has been solved by gamer.
-  var puzzleSolved = function() {
-    setTimeout(shakeElements, 3000, elementsToShake);
-  }
-  // Activate congratulations to user by calling the function.
-  //puzzleSolved();
-
-})()
-
-
 function dragStart(event) {
   event.dataTransfer.setData("Text", event.target.id);
 }
-
 
 function allowDrop(event) {
   event.preventDefault();
@@ -156,3 +211,6 @@ function drop(event) {
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
 }
+
+// Activate congratulations to user by calling the function.
+//puzzleSolved();
