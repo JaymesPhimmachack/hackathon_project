a23778bbaf5ada87fcf9bb910fe4d8678c34dc2c
Joseph Michael Matembu <jmatembu@gmail.com>
2016-12-11 21:45:55 +0300

    Fix puzzle difficulty

--- script.js
+++ script.js
@@ -13,9 +13,7 @@
  * *******************************************************
  */
 
-
-
-/***** Objects *****/
+ /***** Objects *****/
 
 var settings = {};
 
@@ -29,7 +27,7 @@ var elementsToShake = document.querySelectorAll('.piece');
 var buttonsToHide = [easyButton, mediumButton, hardButton, scramble];
 /***** Functions *****/
 
-var hideStartButtons = function(buttons) {
+var hideButtons = function(buttons) {
 
   return buttons.forEach(function(element, index) {
     return element.setAttribute("class", "hide-element");
@@ -93,114 +91,141 @@ hardButton.addEventListener('click', function() {
 
 
 function dragstartHandler(event) {
-  console.log(event);
-  event.dataTransfer.setData("text/plain", event.target.id);
+    console.log(event);
+    event.dataTransfer.setData("text/plain", event.target.id);
 }
+
 
 function dropHandler(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData("text/plain");
-  event.target.appendChild(document.getElementById(data));
+
+    var target44 = { x: 821, y: 453 };
+
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text/plain");
+     window.alert(event.clientX + ',' + event.clientY);
+
+
+    if (event.clientX === target44.x && event.clientX === target44.x) {
+
+       
+        event.target.appendChild(document.getElementById(data));
+        return false;
+
+    }
+
+
 }
 
+
 function dragoverHandler(event) {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = "move";
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
 }
+
+
+
+
 
 // Scramble
 
 function clickHandler(event) {
-  var bg = document.querySelector('#droppableZone');
-  var pieces = [].slice.call(document.querySelectorAll('.piece'));
+    var bg = document.querySelector('#droppableZone');
+    var pieces = [].slice.call(document.querySelectorAll('.piece'));
 
-  bg.setAttribute('class', 'drop-zone unsolved')
+    bg.setAttribute('class', 'drop-zone unsolved')
 
-  pieces.forEach(piece => {
-    piece.style.display = 'block'
-    piece.style.position = 'absolute';
-    piece.style.top = Math.floor(Math.random() * (48+1)) + '%';
-    piece.style.left = Math.floor(Math.random() * (87+1)) + '%';
-    document.querySelector('#pieces-tray').appendChild(piece);
-  });
+    pieces.forEach(piece => {
+        piece.style.display = 'block'
+        piece.style.position = 'absolute';
+        piece.style.top = Math.floor(Math.random() * (48 + 1)) + '%';
+        piece.style.left = Math.floor(Math.random() * (87 + 1)) + '%';
+        document.querySelector('#pieces-tray').appendChild(piece);
+    });
 
-  showControlsAndTimer();
-  hideStartButtons(buttonsToHide);
-  startClock(settings.getDuration());
+    showControlsAndTimer();
+    hideButtons(buttonsToHide);
+    startClock(settings.getDuration());
 }
 
 // Show and Hide Buttons
 function showControlsAndTimer() {
-  var hint = document.getElementById('hint');
-  hint.setAttribute("class", "hint-button");
+    var hint = document.getElementById('hint');
 
-  var reset = document.getElementById('reset');
-  reset.setAttribute("class", "reset-button");
+    if (settings.difficulty !== "hard") {
+      hint.setAttribute("class", "hint-button");
+    }
 
-  var clock = document.getElementById('clock');
-  clock.setAttribute("class", "timer");
+    var reset = document.getElementById('reset');
+    reset.setAttribute("class", "reset-button");
+
+    var clock = document.getElementById('clock');
+    clock.setAttribute("class", "timer");
 }
 
-// function hideScramble() {
-//   var scramble = document.getElementById('scramble');
-//   scramble.setAttribute("class", "hide-element");
-// }
+function hideScramble() {
+    var scramble = document.getElementById('scramble');
+    scramble.setAttribute("class", "hide-element");
+}
 
 // Show and Hide Hint
 function showHint(event) {
-  var puzzle = document.getElementById('solved-hint');
-  puzzle.setAttribute("class", "solved-puzzle solved");
-  event.preventDefault();
+    var puzzle = document.getElementById('solved-hint');
+    puzzle.setAttribute("class", "solved-puzzle solved");
+    event.preventDefault();
 }
 
 function hideHint(event) {
-  var puzzle = document.getElementById('solved-hint');
-  puzzle.setAttribute("class", "hide-element");
-  event.preventDefault();
+    var puzzle = document.getElementById('solved-hint');
+    puzzle.setAttribute("class", "hide-element");
+    event.preventDefault();
 }
 
 // Countdown Clock
 function getTimeRemaining(endTime) {
-  //Time remaining in milliseconds
-  var timeRemaining = endTime - Date.parse(new Date());
-  //gets the remaining seconds and minutes
-  var seconds = Math.floor((timeRemaining/1000) % 60);
-  var minutes = Math.floor(((timeRemaining/1000) / 60) % 60);
+    //Time remaining in milliseconds
+    var timeRemaining = endTime - Date.parse(new Date());
+    //gets the remaining seconds and minutes
+    var seconds = Math.floor((timeRemaining / 1000) % 60);
+    var minutes = Math.floor(((timeRemaining / 1000) / 60) % 60);
 
-  var time = {};
-  time.timeLeft = timeRemaining;
-  time.minutes = minutes;
-  time.seconds = seconds;
+    var time = {};
+    time.timeLeft = timeRemaining;
+    time.minutes = minutes;
+    time.seconds = seconds;
 
-  return time;
+    return time;
 }
 
 function startClock(duration) {
-  var ms = duration * 1000;
-  var startTime = new Date(Date.parse(new Date()) + ms + 1000);
-  var clock = document.getElementById('clock');
+    var ms = duration * 1000;
+    var startTime = new Date(Date.parse(new Date()) + ms + 1000);
+    var clock = document.getElementById('clock');
 
-  var timer = setInterval(function() {
-    var tx = getTimeRemaining(startTime);
+    var timer = setInterval(function() {
+        var tx = getTimeRemaining(startTime);
 
-    var min = (('0' + tx.minutes).slice(-2));
-    var sec = (('0' + tx.seconds).slice(-2));
-    clock.innerHTML = min + ':' + sec;
+        var min = (('0' + tx.minutes).slice(-2));
+        var sec = (('0' + tx.seconds).slice(-2));
+        clock.innerHTML = min + ':' + sec;
 
-    if(tx.timeLeft <= 0) {
-      clearInterval(timer);
-      playSound();
-    }
-  }, 1000);
+        if (tx.timeLeft <= 0) {
+            clearInterval(timer);
+            playSound();
+        }
+    }, 1000);
 }
 
 function playSound() {
-  document.getElementById('audiotag1').play();
+    document.getElementById('audiotag1').play();
 }
 
+
+
+/*
 function dragStart(event) {
   event.dataTransfer.setData("Text", event.target.id);
 }
+
 
 function allowDrop(event) {
   event.preventDefault();
@@ -211,6 +236,7 @@ function drop(event) {
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
 }
+*/
 
 // Activate congratulations to user by calling the function.
 //puzzleSolved();
